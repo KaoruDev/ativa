3b4e36ec58bf946cbc023067db3d56e4df2d83f7
Kaoru <dev@kaoruk.com>
2013-10-15 10:34:20 -0500

    Got RMagick to work, resize :thumb/:medium

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -32,9 +32,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+  version :thumb do
+    # process :scale => [50, 50]
+    process :resize_to_fit => [75, 75]
+  end
+
+  version :medium do
+    # process :scale => [50, 50]
+    process :resize_to_fit => [800, 800]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
